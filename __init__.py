973f77f926ea1954c03e489b9d043fd37a03498c
klevinn <calvinlyn11@gmail.com>
2021-12-22 03:44:48 +0800

    Testing Phase

--- __init__.py
+++ __init__.py
@@ -1,1 +1,10 @@
-print("No")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run()
