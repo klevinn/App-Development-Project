84a2bbe9fec95a6c5fec29129d95705843c6250f
klevinn <calvinlyn11@gmail.com>
2021-12-23 21:17:33 +0800

    Signup page 1 Completed

--- __init__.py
+++ __init__.py
@@ -10,5 +10,9 @@ def home():
 def login():
     return render_template('login.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run()
