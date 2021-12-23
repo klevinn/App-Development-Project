4007cd1f037d866cb4eeccd981258c13d22af13f
klevinn <calvinlyn11@gmail.com>
2021-12-23 20:59:51 +0800

    Made Login Form & Linked Navbars

--- __init__.py
+++ __init__.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run()
