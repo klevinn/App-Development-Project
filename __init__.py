042902d203a8d7d2fb9db2a7f5f3bc613f33ad86
klevinn <calvinlyn11@gmail.com>
2022-02-13 20:08:41 +0800

    Fixed issues

--- __init__.py
+++ __init__.py
@@ -4,7 +4,8 @@ from re import T
 from tkinter import S
 from xml.dom.domreg import registered
 #flash was initially used but instead changed to session for display of messages
-from flask import Flask, render_template, request, redirect, url_for, session, flash, send_from_directory, abort
+from flask import Flask, render_template, request, redirect, url_for, session, flash, send_from_directory, abort, send_file
+import matplotlib.pyplot as plt
 #Shelve for Persistent Storage
 import shelve
 #os for stuff like environment variables
@@ -44,6 +45,7 @@ import User, Staff, Feedback, Customer
 from Security_Validation import validate_card_number, Sanitise, validate_expiry_date, validate_session, validate_session_open_file_admin, validate_session_admin
 #Functions to shorten code
 from Functions import duplicate_email, duplicate_username, get_user_name, check_banned, fix_unit_number, fix_expiry_year, allowed_file, generate_random_password, generate_staff_id, generate_feedback_id, get_file_extension, generate_user_id
+from productgraphgen import graph
 
 #Start Of Web Dev
 app = Flask(__name__)
@@ -82,6 +84,8 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 #For product
 PRODUCTPIC_UPLOAD_PATH = 'static/images/store'
 app.config['UPLOAD_FOLDER_PRODUCT'] = PRODUCTPIC_UPLOAD_PATH
+
+
 
 #Limiter for login security
 limiter = Limiter(app, key_func=get_remote_address)
@@ -1740,6 +1744,25 @@ def staffprod():
     else:
         return redirect(url_for('login'))
 
+@app.route('/graphdownload', methods=['GET', 'POST'])
+def graphdownload():
+    if "staff" in session:
+        StaffName = session["staff"]
+        valid_session, name = validate_session_open_file_admin(StaffName)
+        if valid_session:
+
+            try:
+                graph()
+            except:
+                print("Error")
+
+            return send_file('static/images/graph.png', as_attachment=True)
+        else:
+            session.clear()
+            return redirect(url_for('home'))
+    else:
+        abort(403)
+
 @app.route('/staffupdate/<id>/', methods=['GET', 'POST'])
 #@app.route('/staffupdate/<string:id>/', methods=['GET', 'POST'])
 def staffupdate(id):
@@ -3345,11 +3368,19 @@ def create_consultation():
                 sametime = False
                 samedate = False
                 samedoc = False
+                timelist = []
+                datelist = []
                 for key in customers_dict:
                     customer = customers_dict.get(key)
                     customers_list.append(customer)
                     print("form data is "+ str(create_customer_form.date_joined.data))
                     for customer in customers_list:
+                        ti = customer.get_time()
+                        ti = str(ti)
+                        timelist.append(ti)
+                        date = customer.get_date()
+                        date = str(date)
+                        datelist.append(date)
                         if create_customer_form.date_joined.data == customer.get_date():
                             print("Same Date")
                             samedate = True
@@ -3360,8 +3391,8 @@ def create_consultation():
                                     print("Conflicting Appointment")
                                     samedoc = True
                                     appointment = False
-                                    break
-                                    """
+                                    #break
+                                    
                                     Rtimelist = str(timelist)[1:-2]
                                     Rtimelist = Rtimelist.strip("','")
                                     Rdatelist = str(datelist)[1:-2]
@@ -3382,8 +3413,8 @@ def create_consultation():
 
 
 
-                                    return render_template("user/guest/xuzhi/ErrorDate.html", timelistval = Rtimelist, datelistval = create_customer_form.date_joined.data)
-                                    """
+                                    return render_template("user/guest/xuzhi/ErrorDate.html", timelistval = Rtimelist, datelistval = create_customer_form.date_joined.data, consultactive = True, usersession = True,user = UserName, av=av)
+
                                 else:
                                     appointment = True
                             else:
@@ -3448,11 +3479,19 @@ def create_consultation():
                 sametime = False
                 samedate = False
                 samedoc = False
+                timelist = []
+                datelist = []
                 for key in customers_dict:
                     customer = customers_dict.get(key)
                     customers_list.append(customer)
                     print("form data is "+ str(create_customer_form.date_joined.data))
                     for customer in customers_list:
+                        ti = customer.get_time()
+                        ti = str(ti)
+                        timelist.append(ti)
+                        date = customer.get_date()
+                        date = str(date)
+                        datelist.append(date)
                         if create_customer_form.date_joined.data == customer.get_date():
                             print("Same Date")
                             samedate = True
@@ -3463,8 +3502,8 @@ def create_consultation():
                                     print("Conflicting Appointment")
                                     samedoc = True
                                     appointment = False
-                                    break
-                                    """
+                                    #break
+
                                     Rtimelist = str(timelist)[1:-2]
                                     Rtimelist = Rtimelist.strip("','")
                                     Rdatelist = str(datelist)[1:-2]
@@ -3485,8 +3524,8 @@ def create_consultation():
 
 
 
-                                    return render_template("user/guest/xuzhi/ErrorDate.html", timelistval = Rtimelist, datelistval = create_customer_form.date_joined.data)
-                                    """
+                                    return render_template("user/guest/xuzhi/ErrorDate.html", timelistval = Rtimelist, datelistval = create_customer_form.date_joined.data, consultactive = True, staffsession = True, staff=name)
+
                                 else:
                                     appointment = True
                             else:
@@ -3668,12 +3707,20 @@ def update_consultation(id):
                 sametime = False
                 samedate = False
                 samedoc = False
+                timelist = []
+                datelist = []
                 for key in customer_dict:
                     if customer_dict[key].get_us() != idNumber:
                         customer = customer_dict.get(key)
                         customers_list.append(customer)
                         print("form data is "+ str(update_customer_form.date_joined.data))
                         for customer in customers_list:
+                            ti = customer.get_time()
+                            ti = str(ti)
+                            timelist.append(ti)
+                            date = customer.get_date()
+                            date = str(date)
+                            datelist.append(date)
                             if update_customer_form.date_joined.data == customer.get_date():
                                 print("Same Date as existing")
                                 samedate = True
@@ -3684,19 +3731,19 @@ def update_consultation(id):
                                         print("Conflicting Appointment")
                                         samedoc = True
                                         appointment = False
-                                        break
-                                        """
+                                        #break
+                                        
                                         Rtimelist = str(timelist)[1:-2]
                                         Rtimelist = Rtimelist.strip("','")
                                         Rdatelist = str(datelist)[1:-2]
                                         Rdatelist = Rdatelist.strip('')
                                         timelis = []
-                                        for key in customers_dict:
-                                        customer = customers_dict.get(key)
-                                        customers_list.append(customer)
+                                        for key in customer_dict:
+                                            customer = customer_dict.get(key)
+                                            customers_list.append(customer)
 
                                         bonk = customer.get_date()
-                                        if bonk == create_customer_form.date_joined.data:
+                                        if bonk == update_customer_form.date_joined.data:
                                                 date = customer.get_time()
                                                 timelis.append(date)
                                                 Rtimelist = str(timelis)[1:-2]
@@ -3706,8 +3753,8 @@ def update_consultation(id):
 
 
 
-                                        return render_template("user/guest/xuzhi/ErrorDate.html", timelistval = Rtimelist, datelistval = create_customer_form.date_joined.data)
-                                        """
+                                        return render_template("user/guest/xuzhi/ErrorDate.html", timelistval = Rtimelist, datelistval = update_customer_form.date_joined.data, consultactive = True, usersession = True,user = UserName, av=av)
+                                        
                                     else:
                                         appointment = True
                                 else:
@@ -3812,19 +3859,19 @@ def update_consultation(id):
                                         print("Conflicting Appointment")
                                         samedoc = True
                                         appointment = False
-                                        break
-                                  
+                                        #break
+                                        
                                         Rtimelist = str(timelist)[1:-2]
                                         Rtimelist = Rtimelist.strip("','")
                                         Rdatelist = str(datelist)[1:-2]
                                         Rdatelist = Rdatelist.strip('')
                                         timelis = []
-                                        for key in customers_dict:
-                                        customer = customers_dict.get(key)
-                                        customers_list.append(customer)
+                                        for key in customer_dict:
+                                            customer = customer_dict.get(key)
+                                            customers_list.append(customer)
 
                                         bonk = customer.get_date()
-                                        if bonk == create_customer_form.date_joined.data:
+                                        if bonk == update_customer_form.date_joined.data:
                                                 date = customer.get_time()
                                                 timelis.append(date)
                                                 Rtimelist = str(timelis)[1:-2]
@@ -3834,8 +3881,8 @@ def update_consultation(id):
 
 
 
-                                        return render_template("user/guest/xuzhi/ErrorDate.html", timelistval = Rtimelist, datelistval = create_customer_form.date_joined.data)
-                                      
+                                        return render_template("user/guest/xuzhi/ErrorDate.html", timelistval = Rtimelist, datelistval = update_customer_form.date_joined.data, consultactive = True, staffsession = True, staff=name)
+                                        
                                     else:
                                         appointment = True
                                 else:
